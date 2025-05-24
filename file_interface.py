9a10856c64f2839631ad4337fada2eec1d3755f8
itozt <162090884+itozt@users.noreply.github.com>
2025-05-24 20:14:43 +0700

    Create file_interface.py

---
+++ file_interface.py
@@ -0,0 +1,78 @@
+import os
+import json
+import base64
+from glob import glob
+
+
+class FileInterface:
+    def __init__(self):
+        os.chdir('files/')
+
+    def list(self,params=[]):
+        try:
+            filelist = glob('*.*')
+            return dict(status='OK',data=filelist)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def hapus(self,params=[]):
+        try:
+            filename = params[0]
+            if os.path.exists(filename):
+                os.remove(filename)
+                return dict(status='OK', data=f'{filename} berhasil dihapus')
+            else:
+                return dict(status='ERROR', data='File not found')
+        except Exception as e:
+             return dict(status='ERROR',data=str(e))  
+
+    def upload(self, params=[]):
+        try:
+           filename = params[0]
+           if os.path.exists(filename):
+               return dict(status='OK', data=f'{filename} file sudah ada')
+           filedata_64 = " ".join(params[1:])
+           filedata = base64.b64decode(filedata_64.encode())
+
+           with open(filename, 'wb') as f:
+               f.write(filedata)
+
+               
+
+           return dict(status='OK', data=f'File {filename} berhasil upload')
+        
+        except Exception as e:
+           return dict(status='ERROR', data=str(e))    
+
+    # def get(self,params=[]):
+    #     try:
+    #         filename = params[0]
+    #         if (filename == ''):
+    #             return None
+    #         fp = open(f"{filename}",'rb')
+    #         isifile = base64.b64encode(fp.read()).decode()
+    #         fp = open(filename,'wb+')
+    #         fp.write(isifile)
+    #         fp.cloe()
+    #         return dict(status='OK',data_namafile=filename,data_file=isifile)
+    #     except Exception as e:
+    #         return dict(status='ERROR',data=str(e))
+    
+    def get(self,params=[]):
+        try:
+            filename = params[0]
+            if (filename == ' '):
+                return None
+            with open(filename,'rb') as fp:
+                isifile = base64.b64encode(fp.read()).decode()
+                return dict(status='OK',data_namafile=filename,data_file=isifile)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+
+
+
+if __name__=='_main_':
+    f = FileInterface()
+    print(f.list())
+    print(f.get(['pokijan.jpg']))
